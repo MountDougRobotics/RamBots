343cac840faadea8d3fe163501afe64d3ba50037
LordoCreations <lordocreations@gmail.com>
2023-08-23 19:17:25 -0700

    random docs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chasis/4WD.kt
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.chasis
-
-class `4WD` {
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/drive.kt
@@ -0,0 +1,13 @@
+/**
+ * drive.kt
+ * Purpose: Used to control the movement of the main drive
+ * TODO OMNIDRIVE (360° control)
+ * TODO Routing
+ *
+ **/
+
+
+package org.firstinspires.ftc.teamcode.movement
+
+class drive {
+}
