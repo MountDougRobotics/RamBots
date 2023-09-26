a89caaa667dff7b0aae5ef1d4c3e92f7876a1ceb
LordoCreations <lordocreations@gmail.com>
2023-09-26 07:56:57 -0700

    MF forgor to add roadrunner :skull:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/meta/BotComponents.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/meta/BotComponents.kt
@@ -6,19 +6,20 @@ package org.firstinspires.ftc.teamcode.components.meta
 import com.qualcomm.hardware.rev.RevColorSensorV3
 import ftc.rogue.blacksmith.BlackOp.Companion.hwMap
 import ftc.rogue.blacksmith.util.kt.invoke
-//import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive
-import org.firstinspires.ftc.teamcode.components.hardware.drivetrain
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive
+import org.firstinspires.ftc.teamcode.components.hardware.*
+import org.firstinspires.ftc.teamcode.components.vision.VisionPortal
 
 
 abstract class BaseBotComponents {
-//    val claw   = Claw()
+    val claw   = Claw()
 //    val intake = Intake()
 //    val arm    = Arm()
 //    val wrist  = Wrist()
 //    val lift   = Lift()
 
     open fun updateComponents(useLiftDeadzone: Boolean) {
-//        claw.update()
+        claw.update()
 //        arm.update()
 //        wrist.update()
     }
@@ -40,20 +41,20 @@ data class TeleOpBotComponents(
     }
 }
 
-//fun createAutoBotComponents() =
-//    AutoBotComponents(
-//        SampleMecanumDrive(hwMap),
-//        Camera(),
-//    )
-//
-//data class AutoBotComponents(
-//    val drive: SampleMecanumDrive,
-//    val camera: Camera,
-//) : BaseBotComponents() {
-//    override fun updateComponents(useLiftDeadzone: Boolean) {
-//        super.updateComponents(useLiftDeadzone)
-//        camera.update()
-//        drive.update()
-//        lift.updateAutoLiftNormalPID()
-//    }
-//}
+fun createAutoBotComponents() =
+    AutoBotComponents(
+        SampleMecanumDrive(hwMap),
+        VisionPortal(),
+    )
+
+data class AutoBotComponents(
+    val drive: SampleMecanumDrive,
+    val camera: VisionPortal,
+) : BaseBotComponents() {
+    override fun updateComponents(useLiftDeadzone: Boolean) {
+        super.updateComponents(useLiftDeadzone)
+        //VisionPortal.update()
+        drive.update()
+        //lift.updateAutoLiftNormalPID()
+    }
+}
