7601209c116865be787dbb4565278ac2c8f4999d
LordoCreations <lordocreations@gmail.com>
2023-09-29 14:36:37 -0700

    goofiest fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/testOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/testOp.kt
@@ -5,9 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import ftc.rogue.blacksmith.BlackOp
 import org.firstinspires.ftc.teamcode.opmodes.baseOpMode
 
-@TeleOp(name = "Test Op 1.0")
-class testOp : BlackOp() {
-    override fun go() {
+@TeleOp //(name = "Test Op 1.0")
+class testOp : baseOpMode() {
+//    override fun go() {
+//        TODO("Not yet implemented")
+//    }
+
+    override fun describeControls() {
         TODO("Not yet implemented")
     }
 
