7a7949de5d149fc72c11a06134fc4ab36177bd8d
LordoCreations <lordocreations@gmail.com>
2023-09-29 14:10:03 -0700

    goofiest fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.roadrunner.drive;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 // ! Configured Using LRR (TorqueNADO Standard, 16in TW, 96mm Mecanums)
@@ -94,6 +95,10 @@ public class DriveConstants {
     public static double MAX_ANG_VEL = Math.toRadians(90.3560625);
     public static double MAX_ANG_ACCEL = Math.toRadians(90.3560625);
 
+    public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
+            RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    public static RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR =
+            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
     public static double encoderTicksToInches(double ticks) {
         return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/testOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/testOp.kt
@@ -1,22 +1,15 @@
 package org.firstinspires.ftc.teamcode.tests
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import ftc.rogue.blacksmith.BlackOp
 import org.firstinspires.ftc.teamcode.opmodes.baseOpMode
 
-
-@TeleOp(name = "Test Op Mode w/ BlackSmith")
-class testOp : baseOpMode() {
-    private fun describeDriverControls() = with(bot) {
+@TeleOp(name = "Test Op 1.0")
+class testOp : BlackOp() {
+    override fun go() {
+        TODO("Not yet implemented")
     }
 
-
-    private fun describeCodriverControls() = with(bot) {
-    }
-
-
-
-    override fun describeControls() {
-        describeCodriverControls()
-        describeDriverControls()
-    }
 }
+
