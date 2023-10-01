ece0935a0b38636cd61b1a23f1a32ce409d1181b
LordoCreations <lordocreations@gmail.com>
2023-09-30 19:50:01 -0700

    if no work ima branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/testOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/testOp.kt
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import ftc.rogue.blacksmith.BlackOp
 import org.firstinspires.ftc.teamcode.opmodes.baseOpMode
 
-@TeleOp //(name = "Test Op 1.0")
+@TeleOp(name = "Test Op 1.0")
 class testOp : baseOpMode() {
     override fun describeControls() {
         TODO("Not yet implemented")
