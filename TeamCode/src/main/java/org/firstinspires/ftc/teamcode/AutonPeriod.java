70b6dbd882651e09d34668e08107abf2361feae7
Qirjako <689805@sd61.bc.ca>
2023-12-01 15:43:55 -0800

    Added AutonPeriod class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonPeriod.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+//Autonomous Class
+public class AutonPeriod {
+
+    @Override private void runOpMode {
+
+    } //runOpMode
+
+} //ControlPeriod
